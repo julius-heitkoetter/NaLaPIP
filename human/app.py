1accc01fa15db00805d88eb4ef0a952f1e04edaf
Julius Heitkoetter <juliush@mit.edu>
2023-12-11 19:52:08 +0000

    added basic app for human input

---
+++ human/app.py
@@ -0,0 +1,136 @@
+from flask import request, jsonify, render_template
+from flask import Flask
+from flask_cors import CORS
+
+from datetime import datetime
+import csv
+import os
+import argparse
+import json
+import csv
+import random
+
+DIR_EXPERIMENTS = "experiments"
+DIR_HUMAN = "human"
+DIR_RAWDATA = "raw_responses"
+
+DIR_DATA = "data"
+
+class FlaskAppWrapper(object):
+
+    def __init__(self, app, experiment_id, input_file_name, num_question_per_user, **configs):
+        
+        #Initialize
+        self.app = app
+        self.configs(**configs)
+
+        self.assigning_user_number = 0
+
+        self.experiment_id = experiment_id
+        self.saving_loc = os.path.join(DIR_EXPERIMENTS, self.experiment_id, DIR_HUMAN)
+        self.raw_saving_loc = os.path.join(DIR_EXPERIMENTS, self.experiment_id, DIR_HUMAN, DIR_RAWDATA)
+        os.makedirs(self.saving_loc, exist_ok=True)
+        os.makedirs(self.raw_saving_loc, exist_ok=True)
+
+        # load in the data
+        total_input_data = []
+        with open(os.path.join(DIR_DATA, input_file_name), 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                total_input_data.append(row)
+        random.shuffle(total_input_data)
+        
+        # split up the data into chunks that the user can handle
+        m = num_question_per_user
+        sub_lists = [total_input_data[i:i + m] for i in range(0, len(total_input_data), m)]
+        self.input_data = sub_lists
+
+        # enable CORS:
+        CORS(self.app)
+
+        # add endpoints for flask app
+        self.add_endpoint('/api/heartbeat', 'heartbeat', self.heartbeat, methods=["GET"])
+        self.add_endpoint('/api/send_response', 'send_response', self.send_response, methods=["POST"])
+        self.add_endpoint('/', 'index', self.index)
+
+        
+    def configs(self, **configs):
+        for config, value in configs:
+            self.app.config[config.upper()] = value
+
+    def add_endpoint(self, endpoint = None, endpoint_name = None, handler = None, methods = ['GET'], *args, **kwargs):
+        self.app.add_url_rule(endpoint, endpoint_name, handler, methods = methods, *args, **kwargs)
+
+    def run(self, **kwargs):
+        self.app.run(**kwargs)
+
+    def index(self):
+        
+        self.assigning_user_number += 1
+
+        data_for_user = self.input_data[self.assigning_user_number % len(self.input_data)]
+
+        return render_template(
+            'index.html', 
+            user_number = self.assigning_user_number, 
+            stimuli_batch = self.input_data
+        )
+        
+    def heartbeat(self):
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    def send_response(self):
+        
+        data = request.json.get('data')["trials"]
+        user_number = request.json.get('user_number')
+
+        #save raw data
+        file_name = os.path.join(self.raw_saving_loc, "rawResponse" + str(user_number) + ".json")
+        with open(file_name, 'w') as file:
+            json.dump(data, file)
+
+        file_name = os.path.join(self.saving_loc, "human_unaggregated_results.csv")
+        file_exists = os.path.isfile(file_name)
+        with open(file_name, mode='a' if file_exists else 'w', newline='') as file:
+            writer = csv.writer(file)
+
+            # If the file did not exist, write the header first
+            if not file_exists:
+                writer.writerow(['task_id', 'runtime', 'rating'])
+
+            for row in data:
+                if row["trial_type"] == 'survey-likert':
+                    task_id = row["task_id"]
+                    rating = row["response"]["Q0"] + 1
+                    time = row["time_elapsed"] / 10000
+
+                    new_row = [task_id, time, rating]
+
+                    # Write the data row
+                    writer.writerow(new_row)
+
+
+        return jsonify({
+            'response': "OK",
+        })
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("experiment_id")
+    parser.add_argument("input_file_name")
+    parser.add_argument("--port", type=int, default=3210, help="Which port to run the app on.")
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="Which host to run the app on.")
+    parser.add_argument("--num_question_per_user", type=int, default=24, help="How many questions each user is presented with")
+    args = parser.parse_args()
+
+    app = FlaskAppWrapper(Flask(
+        __name__,
+        template_folder="/data/julius_heitkoetter/NaLaPIP/human",
+        static_folder="/data/julius_heitkoetter/NaLaPIP/human/static",
+    ), args.experiment_id, args.input_file_name, args.num_question_per_user)
+    app.run(debug=True,port=args.port, host=args.host)
+
+if __name__ == "__main__":
+    main()
