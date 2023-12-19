c8da87e316b7c098a9a0b4d68714ea4fd6d62855
Julius Heitkoetter <juliush@mit.edu>
2023-12-19 16:20:19 +0000

    added rest of Latex documents and things needed to compile paper

---
+++ paper/compile.sh
@@ -0,0 +1,21 @@
+
+# Put everythong in a compile directory
+mkdir compile
+cp -r * compile/.
+cd compile
+
+# Add Latexmk file as excecutable
+chmod latexmk 744
+EXPORT PATH=${PATH}:${BASE}/paper2/compile/latexmk
+
+#Expand apacite
+tex apacite.ins
+
+latexmk -f -pdf > compile.log
+
+cd ..
+
+cp compile/main.pdf output/.
+cp compile/compile.log output/.
+
+rm -rf compile
